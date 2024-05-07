d9b6a4c8711f00b02e5985f5869dde6381c83c32
Laraib <laraibks@gmail.com>
2024-05-08 02:54:57 +0530

    init

---
+++ 1Nodejs-TheBeginning/HelloNode.js/index.js
@@ -0,0 +1,11 @@
+console.log('Hello World!');
+
+function add(a,b){
+    return a+b;
+}
+console.log(add(2,2));
+
+console.log(process.argv);
+
+var args=process.argv.slice(2);
+console.log("Adding the number: ", add(parseInt(args[0]), parseInt(args[1])));
